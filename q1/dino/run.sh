7d72fb84072a41f2c421ba2a0976f21ac7530b23
Gaurav Behera <gaurav.behera@research.iiit.ac.in>
2025-04-21 04:12:01 +0530

    [+]CIFAR-10 ViT

---
+++ q1/dino/run.sh
@@ -0,0 +1,7 @@
+python visualize_attention.py \
+    --output_dir ./output/1/ \
+    --image_path ./input_images/1.png \
+
+python visualize_attention.py \
+    --output_dir ./output/2/ \
+    --image_path ./input_images/2.png
